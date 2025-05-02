e8b828b7093952c5433f79be8cba24fc0717a360
hseipp <20093686+hseipp@users.noreply.github.com>
2025-05-02 17:53:21 +0200

    For s390x, IPv6 needs to be disabled.
    Signed-off-by: hseipp <20093686+hseipp@users.noreply.github.com>

--- build_s390x.sh
+++ build_s390x.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp mqlicense_s390x.sh mqlicense.sh
 rm -rf lap && cp -r lap_s390x lap
-buildah bud --platform=linux/s390x -t cphtestp:s390x
+buildah bud --platform=linux/s390x --network slirp4netns:enable_ipv6=false -t cphtestp:s390x
